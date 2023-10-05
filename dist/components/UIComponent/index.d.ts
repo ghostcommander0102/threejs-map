bcde22e164c23aedaffe223ff6c8931b6c4f0ee6
DESKTOP-3HI6P6M\aleks <aleksmazur12@gmail.com>
2023-10-05 20:45:07 +0300

    clean-up

--- dist/components/UIComponent/index.d.ts
+++ dist/components/UIComponent/index.d.ts
@@ -1,12 +1,12 @@
-import { IAmenitiesInteractiveList, IFloorSelectorProps, TMapMode } from "types";
+import { IAmenitiesInteractiveList, IFloorSelectorProps, TRoles } from "src/types";
 import { MouseEventHandler } from "react";
-declare const UIComponent: ({ floors, accentColor, selectedFloorIndex, handleFloorChange, amenitiesList, zoomIn, zoomOut, reset, clearRoute, handleAmenityClick, mode, }: IFloorSelectorProps & {
+declare const UIComponent: ({ floors, accentColor, selectedFloorIndex, handleFloorChange, amenitiesList, zoomIn, zoomOut, reset, clearRoute, handleAmenityClick, role, }: IFloorSelectorProps & {
     amenitiesList: IAmenitiesInteractiveList[];
     zoomIn?: MouseEventHandler<HTMLDivElement> | undefined;
     zoomOut?: MouseEventHandler<HTMLDivElement> | undefined;
     reset?: (() => void) | undefined;
     clearRoute?: MouseEventHandler<HTMLDivElement> | undefined;
     handleAmenityClick: (type: string) => void;
-    mode?: TMapMode | undefined;
+    role?: TRoles | undefined;
 }) => import("react/jsx-runtime").JSX.Element;
 export default UIComponent;
