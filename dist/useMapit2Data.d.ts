d0181a181fb86107dd11f5d866a48179fe382718
DESKTOP-3HI6P6M\aleks <aleksmazur12@gmail.com>
2023-11-09 20:13:08 +0200

    update-build

--- dist/useMapit2Data.d.ts
+++ dist/useMapit2Data.d.ts
@@ -8,6 +8,7 @@ interface useMapIt2DataProps {
  * This is a hook that fetches the mapit2 data from the server by CENTER_ID. or uses the data passed in.
  * @param CENTER_ID
  * @param mapitData
+ * @param APIUri
  */
 export declare function useMapit2Data({ CENTER_ID, mapitData, APIUri }: useMapIt2DataProps): MapIt2Response | null;
 export {};
